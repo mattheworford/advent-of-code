e0e41118e8dcf921cb501acf20072b5348400003
Matthew Orford <mattorford9@gmail.com>
2023-12-08 15:32:17 -0500

    day5 part2

--- 2023/day5/day5.go
+++ 2023/day5/day5.go
@@ -2,23 +2,70 @@ package day5
 
 import (
 	"bufio"
-	"fmt"
 	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
-func GetLowestLocation(documentName string) int {
+func GetLowestLocationOfSeedRanges(documentName string) int {
 	file, _ := os.Open(documentName)
 	scanner := bufio.NewScanner(file)
 	scanner.Scan()
 	seeds := atoi(strings.Fields(strings.Split(scanner.Text(), ": ")[1]))
-	fmt.Println(seeds)
+	seedRanges := getSeedRanges(seeds)
 	scanner.Scan()
 	maps := map[string]Map{}
 	for scanner.Scan() {
-		source, m := parseMap(scanner)
+		_, destination, m := parseMap(scanner)
+		maps[destination] = m
+	}
+	for i := 0; true; i++ {
+		seed := getSeed(i, maps)
+		for _, seedRange := range seedRanges {
+			if seedRange.contains(seed) {
+				return i
+			}
+		}
+	}
+	return 0
+}
+
+func getSeedRanges(seeds []int) (seedRanges []SeedRange) {
+	for i, seed := range seeds {
+		if i%2 == 0 {
+			seedRanges = append(seedRanges, SeedRange{
+				start:  seed,
+				length: seeds[i+1],
+			})
+		}
+	}
+	return
+}
+
+func getSeed(location int, maps map[string]Map) int {
+	destination := "location"
+	source := location
+	for i := 0; i < 7; i++ {
+		m, exists := maps[destination]
+		if !exists {
+			break
+		}
+		source = m.getSource(source)
+		destination = m.source
+	}
+	return source
+}
+
+func GetLowestLocationOfSeeds(documentName string) int {
+	file, _ := os.Open(documentName)
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	seeds := atoi(strings.Fields(strings.Split(scanner.Text(), ": ")[1]))
+	scanner.Scan()
+	maps := map[string]Map{}
+	for scanner.Scan() {
+		source, _, m := parseMap(scanner)
 		maps[source] = m
 	}
 	lowestLocation := math.MaxInt
@@ -31,7 +78,7 @@ func GetLowestLocation(documentName string) int {
 func getLocation(seed int, maps map[string]Map) int {
 	source := "seed"
 	destination := seed
-	for {
+	for i := 0; i < 7; i++ {
 		m, exists := maps[source]
 		if !exists {
 			break
@@ -42,25 +89,11 @@ func getLocation(seed int, maps map[string]Map) int {
 	return destination
 }
 
-func (m Map) getDestination(source int) int {
-	for _, r := range m.ranges {
-		if r.inSourceRange(source) {
-			return r.destRangeStart + (source - r.sourceRangeStart)
-		}
-	}
-	return source
-}
-
-func (r Ranges) inSourceRange(source int) bool {
-	fromStart := source - r.sourceRangeStart
-	return 0 <= fromStart && fromStart <= r.length
-}
-
-func parseMap(scanner *bufio.Scanner) (string, Map) {
+func parseMap(scanner *bufio.Scanner) (string, string, Map) {
 	mapType := strings.Fields(scanner.Text())[0]
 	source := strings.Split(mapType, "-")[0]
 	destination := strings.Split(mapType, "-")[2]
-	var ranges []Ranges
+	var ranges []MapRanges
 	for scanner.Scan() {
 		line := scanner.Text()
 		if line == "" {
@@ -69,14 +102,14 @@ func parseMap(scanner *bufio.Scanner) (string, Map) {
 			destRangeStart, _ := strconv.Atoi(strings.Fields(line)[0])
 			sourceRangeStart, _ := strconv.Atoi(strings.Fields(line)[1])
 			length, _ := strconv.Atoi(strings.Fields(line)[2])
-			ranges = append(ranges, Ranges{
+			ranges = append(ranges, MapRanges{
 				destRangeStart:   destRangeStart,
 				sourceRangeStart: sourceRangeStart,
 				length:           length,
 			})
 		}
 	}
-	return source, Map{
+	return source, destination, Map{
 		source:      source,
 		destination: destination,
 		ranges:      ranges,
@@ -94,11 +127,49 @@ func atoi(strings []string) (ints []int) {
 type Map struct {
 	source      string
 	destination string
-	ranges      []Ranges
+	ranges      []MapRanges
 }
 
-type Ranges struct {
+func (m Map) getDestination(source int) int {
+	for _, r := range m.ranges {
+		if r.inSourceRange(source) {
+			return r.destRangeStart + (source - r.sourceRangeStart)
+		}
+	}
+	return source
+}
+
+func (m Map) getSource(destination int) int {
+	for _, r := range m.ranges {
+		if r.inDestinationRange(destination) {
+			return r.sourceRangeStart + (destination - r.destRangeStart)
+		}
+	}
+	return destination
+}
+
+type MapRanges struct {
 	destRangeStart   int
 	sourceRangeStart int
 	length           int
+}
+
+func (r MapRanges) inSourceRange(source int) bool {
+	fromStart := source - r.sourceRangeStart
+	return 0 <= fromStart && fromStart <= r.length
+}
+
+func (r MapRanges) inDestinationRange(destination int) bool {
+	fromStart := destination - r.destRangeStart
+	return 0 <= fromStart && fromStart <= r.length
+}
+
+type SeedRange struct {
+	start  int
+	length int
+}
+
+func (r SeedRange) contains(seed int) bool {
+	fromStart := seed - r.start
+	return 0 <= fromStart && fromStart <= r.length
 }
--- 2023/day5/day5_test.go
+++ 2023/day5/day5_test.go
@@ -7,14 +7,26 @@ import (
 	"testing"
 )
 
-func TestGetLowestLocation(t *testing.T) {
+func TestGetLowestLocationOfSeeds(t *testing.T) {
 	testFiles, err := os.ReadDir("test_data")
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return
 	}
 	for _, testFile := range testFiles {
-		actual := GetLowestLocation("test_data/" + testFile.Name())
+		actual := GetLowestLocationOfSeeds("test_data/" + testFile.Name())
+		t.Logf(strconv.Itoa(actual))
+	}
+}
+
+func TestGetLowestLocationOfSeedRanges(t *testing.T) {
+	testFiles, err := os.ReadDir("test_data")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	for _, testFile := range testFiles {
+		actual := GetLowestLocationOfSeedRanges("test_data/" + testFile.Name())
 		t.Logf(strconv.Itoa(actual))
 	}
 }
