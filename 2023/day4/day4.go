e0998041698c42da2d290438dce52be61a5c4256
Matthew Orford <mattorford9@gmail.com>
2023-12-08 00:13:31 -0500

    day4 part2

--- 2023/day4/day4.go
+++ 2023/day4/day4.go
@@ -2,11 +2,70 @@ package day4
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"regexp"
 	"strconv"
 	"strings"
 )
+
+func GetTotalScratchcards(documentName string) int {
+	file, _ := os.Open(documentName)
+	scanner := bufio.NewScanner(file)
+	cardNumber := 1
+	cardToMatches := map[int]int{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		if containsValidCardInfo(line) {
+			record := strings.Split(line, ": ")[1]
+			cardToMatches[cardNumber] = getTotalMatches(record)
+		}
+		cardNumber += 1
+	}
+	fmt.Println(cardToMatches)
+	return getCardsToCopies(cardToMatches)
+}
+
+func getCardsToCopies(cardToMatches map[int]int) int {
+	cardToCopies := map[int]int{}
+	for i := 1; i <= len(cardToMatches); i++ {
+		cardToCopies = initOrAdd(cardToCopies, i, 1)
+		for j := 1; j <= cardToMatches[i]; j++ {
+			cardToCopies = initOrAdd(cardToCopies, i+j, cardToCopies[i])
+		}
+	}
+	return sumValues(cardToCopies)
+}
+
+func sumValues(m map[int]int) (sum int) {
+	for _, value := range m {
+		sum += value
+	}
+	return
+}
+
+func initOrAdd(m map[int]int, key, value int) map[int]int {
+	_, exists := m[key]
+	if exists {
+		m[key] += value
+	} else {
+		m[key] = value
+	}
+	return m
+}
+
+func getTotalMatches(card string) (copies int) {
+	columns := strings.Split(card, "|")
+	winningNumbers := parseNumbers(columns[0])
+	actualNumbers := parseNumbers(columns[1])
+	for number, _ := range actualNumbers {
+		_, isWinning := winningNumbers[number]
+		if isWinning {
+			copies += 1
+		}
+	}
+	return
+}
 
 func GetCardPointValues(documentName string) int {
 	file, _ := os.Open(documentName)
--- 2023/day4/day4_test.go
+++ 2023/day4/day4_test.go
@@ -7,6 +7,18 @@ import (
 	"testing"
 )
 
+func TestGetTotalScratchcards(t *testing.T) {
+	testFiles, err := os.ReadDir("test_data")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	for _, testFile := range testFiles {
+		actual := GetTotalScratchcards("test_data/" + testFile.Name())
+		t.Logf(strconv.Itoa(actual))
+	}
+}
+
 func TestGetGearRatiosSum(t *testing.T) {
 	testFiles, err := os.ReadDir("test_data")
 	if err != nil {
