63a51637189498187ab8f1ab60ed7433f954932d
Matthew Orford <mattorford9@gmail.com>
2023-12-09 00:42:39 -0500

    day 8 part 2

--- 2023/day8/day8.go
+++ 2023/day8/day8.go
@@ -2,11 +2,66 @@ package day8
 
 import (
 	"bufio"
+	"math"
 	"os"
 	"strings"
 )
 
-func GetRequiredSteps(documentName string) (steps int) {
+func GetRequiredStepsFromAllANodes(documentName string) (steps int) {
+	file, _ := os.Open(documentName)
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	instructions := parseInstructions(scanner.Text())
+	scanner.Scan()
+	network := map[string][]string{}
+	var currNodes []string
+	for scanner.Scan() {
+		node, nextElements := parseNode(scanner.Text())
+		if node[2] == 'A' {
+			currNodes = append(currNodes, node)
+		}
+		network[node] = nextElements
+	}
+	countsMap := map[string]int{}
+	for _, currNode := range currNodes {
+		currSteps := 0
+		nextNode := currNode
+		for i := 0; nextNode[2] != 'Z'; i = (i + 1) % len(instructions) {
+			nextNode = getNextElement(nextNode, instructions[i], network)
+			currSteps += 1
+		}
+		countsMap[currNode] = currSteps
+	}
+	return getCombinedLcm(getValues(countsMap))
+}
+
+func getCombinedLcm(nums []int) int {
+	lcm := 1
+	for _, num := range nums {
+		lcm = getLcm(lcm, num)
+	}
+	return lcm
+}
+
+func getLcm(a, b int) (lcm int) {
+	return int(math.Abs(float64(a*b)) / float64(getGcd(a, b)))
+}
+
+func getGcd(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	return getGcd(b, a%b)
+}
+
+func getValues(m map[string]int) (values []int) {
+	for _, value := range m {
+		values = append(values, value)
+	}
+	return
+}
+
+func GetRequiredStepsFromAAA(documentName string) (steps int) {
 	file, _ := os.Open(documentName)
 	scanner := bufio.NewScanner(file)
 	scanner.Scan()
@@ -18,9 +73,8 @@ func GetRequiredSteps(documentName string) (steps int) {
 		network[node] = nextElements
 	}
 	currNode := "AAA"
-	for i := 0; currNode != "ZZZ"; i++ {
-		i = i % len(instructions)
-		currNode = network[currNode][instructions[i]]
+	for i := 0; currNode != "ZZZ"; i = (i + 1) % len(instructions) {
+		currNode = getNextElement(currNode, instructions[i], network)
 		steps += 1
 	}
 	return
@@ -43,4 +97,8 @@ func parseNode(line string) (string, []string) {
 	leftElement := strings.Split(nextElements[1:len(nextElements)-1], ", ")[0]
 	rightElement := strings.Split(nextElements[1:len(nextElements)-1], ", ")[1]
 	return node, []string{leftElement, rightElement}
+}
+
+func getNextElement(currNode string, nextInstruction int, network map[string][]string) string {
+	return network[currNode][nextInstruction]
 }
--- 2023/day8/day8_test.go
+++ 2023/day8/day8_test.go
@@ -7,14 +7,26 @@ import (
 	"testing"
 )
 
-func TestGetRequiredSteps(t *testing.T) {
+func TestGetRequiredStepsFromAllANodes(t *testing.T) {
 	testFiles, err := os.ReadDir("test_data")
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return
 	}
 	for _, testFile := range testFiles {
-		actual := GetRequiredSteps("test_data/" + testFile.Name())
+		actual := GetRequiredStepsFromAllANodes("test_data/" + testFile.Name())
+		t.Logf(strconv.Itoa(actual))
+	}
+}
+
+func TestGetRequiredStepsFromAAA(t *testing.T) {
+	testFiles, err := os.ReadDir("test_data")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	for _, testFile := range testFiles {
+		actual := GetRequiredStepsFromAAA("test_data/" + testFile.Name())
 		t.Logf(strconv.Itoa(actual))
 	}
 }
